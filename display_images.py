f62979e5574c9daaa53e7f3fc5d89967eb21c8bd
ayanacharyya <acharyyaayan@gmail.com>
2024-07-15 13:45:09 +0200

    debugged

--- display_images.py
+++ display_images.py
@@ -15,14 +15,17 @@ start_time = datetime.now()
 if __name__ == "__main__":
 
     df=pd.read_table('/Users/acharyya/Work/astro/passage/passage_data/Par050/id_list_spatially_resolved_galaxies_Par50.txt', comment='#', names=['id'])
+    image_path = '/Users/acharyya/Work/astro/passage/passage_output/Par050/'
+
     count = 0
-    for id in df['id'].values:
+    for this_id in df['id'].values:
         try:
-            im=Image.open(f'/Users/acharyya/Work/astro/passage/passage_output/Par050/{id}/Par050_{id}.line.png')
+            this_id = f'{int(this_id.strip()):05d}'
+            im=Image.open(image_path + f'{this_id}/Par050_{this_id}.line.png')
             im.show()
-            count+=1
-            print(id, count)
-        except:
+            count += 1
+            print(this_id, count)
+        except (FileNotFoundError, ValueError) as e:
             pass
 
     print(f'Completed in {timedelta(seconds=(datetime.now() - start_time).seconds)}')
