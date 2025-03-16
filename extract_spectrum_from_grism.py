abac3efe6489219608553c54ac32d68d527a04a0
ayanacharyya <acharyyaayan@gmail.com>
2025-03-16 20:39:10 +0100

    can plot cutouts of direct image from clear and grism files

--- extract_spectrum_from_grism.py
+++ extract_spectrum_from_grism.py
@@ -49,21 +49,56 @@ def make_ax_labels(ax, xlabel, ylabel, fontsize, hide_xaxis=False, hide_yaxis=Fa
     return ax
 
 # --------------------------------------------------------------------------------------------------------------------
-def cutout2d_and_plot(ra, dec, direct_data, direct_wcs, ax, args, cmap='Greys', hide_xaxis=False, hide_yaxis=False, hide_cbar=False):
+def cutout_spectra_and_plot(ra, dec, orient, data, wcs, ax, args, cmap='Greys'):
     '''
     Cutout a rectangular region around (x,y) from a bigger image (direct_data) and plot it on a given axis
     Returns axis handle
     '''
-    size = args.arcsec_limit * u.arcsec  # 3 arcsec so that it leads to shape 125 x 125 pixels
-    pos = SkyCoord(*(np.array([ra, dec]) * u.deg), frame='fk5')
-    cutout = Cutout2D(direct_data, pos, size, wcs=direct_wcs).data
+    zero_order_shift_pix = 10 # this should be a function of the filter
+    lambda_extent_pix = 200 # this should be a function of the filter wavelength extent
+    spatial_extent_pix = 30 # this should be a function of arcsec.limit
+
+    pos_sky = SkyCoord(*(np.array([ra, dec]) * u.deg), frame='fk5')
+    pos_pix = PixCoord.from_sky(pos_sky, wcs)
+
+    if orient == 'r':
+        npix_x = lambda_extent_pix
+        npix_y = spatial_extent_pix
+        center_pix = [pos_pix.xy[0] + zero_order_shift_pix + npix_x/2, pos_pix.xy[1]]
+    elif orient == 'c':
+        npix_x = spatial_extent_pix
+        npix_y = lambda_extent_pix
+        center_pix = [pos_pix.xy[1], pos_pix.xy[1] + zero_order_shift_pix + lambda_extent_pix/2]
+
+    cutout = Cutout2D(data, center_pix, [npix_x, npix_y]).data
     cutout = np.log10(cutout)
 
     good_data = np.ma.compressed(np.ma.masked_where(~np.isfinite(cutout), cutout))
     vmin = np.percentile(good_data, 1)
     vmax = np.percentile(good_data, 99)
 
-    p = ax.imshow(cutout, cmap=cmap, origin='lower', extent=args.extent, alpha=1, vmin=vmin, vmax=vmax)
+    if orient == 'r': cutout = cutout.T # transposing
+    ax.imshow(cutout, cmap=cmap, origin='lower', vmin=vmin, vmax=vmax)
+    ax = make_ax_labels(ax, '', '', args.fontsize,  hide_xaxis=True, hide_yaxis=True, hide_cbar=True)
+
+    return ax
+
+# --------------------------------------------------------------------------------------------------------------------
+def cutout2d_and_plot(ra, dec, data, wcs, ax, args, cmap='Greys', hide_xaxis=False, hide_yaxis=False, hide_cbar=False):
+    '''
+    Cutout a rectangular region around (x,y) from a bigger image (direct_data) and plot it on a given axis
+    Returns axis handle
+    '''
+    size = args.arcsec_limit * u.arcsec
+    pos = SkyCoord(*(np.array([ra, dec]) * u.deg), frame='fk5')
+    cutout = Cutout2D(data, pos, size, wcs=wcs).data
+    cutout = np.log10(cutout)
+
+    good_data = np.ma.compressed(np.ma.masked_where(~np.isfinite(cutout), cutout))
+    vmin = np.percentile(good_data, 1)
+    vmax = np.percentile(good_data, 99)
+
+    p = ax.imshow(cutout, cmap=cmap, origin='lower', extent=args.extent, vmin=vmin, vmax=vmax)
     ax.scatter(0, 0, marker='x', s=10, c='grey')
 
     if 'pa_arr' in args: ax = annotate_PAs(args.pa_arr, ax, fontsize=args.fontsize)
@@ -86,7 +121,7 @@ if __name__ == "__main__":
 
     filter = 'f200w'
     line = 'Ha'
-    dirimg_cmap = 'cividis'
+    dirimg_cmap, grism_dir_cmap, grism_2d_cmap = 'cividis', 'winter', 'winter'
 
     if phot_catalog_file.exists(): df_photcat = Table.read(phot_catalog_file).to_pandas()
     else: df_photcat = None
@@ -110,19 +145,21 @@ if __name__ == "__main__":
         extract_dir.mkdir(parents=True, exist_ok=True)
 
         # --------setting up full figure----------------------------------
-        nrow, ncol = 4, 8
+        nrow, ncol = 4, 9
         fig = plt.figure(figsize=(12, 6), layout='constrained') # layout = 'tight' or 'constrained'
 
         ax_dirimg = plt.subplot2grid(shape=(nrow, ncol), loc=(0, 0), colspan=2, rowspan=2)
-        ax_grism_raw_c = plt.subplot2grid(shape=(nrow, ncol), loc=(0, 2), colspan=3, rowspan=1)
-        ax_grism_raw_r = plt.subplot2grid(shape=(nrow, ncol), loc=(1, 2), colspan=3, rowspan=1)
-        ax_contfit = plt.subplot2grid(shape=(nrow, ncol), loc=(0, 5), colspan=3, rowspan=2)
+        ax_grism_dir_c = plt.subplot2grid(shape=(nrow, ncol), loc=(0, 2), colspan=1, rowspan=1)
+        ax_grism_dir_r = plt.subplot2grid(shape=(nrow, ncol), loc=(1, 2), colspan=1, rowspan=1)
+        ax_grism_2d_c = plt.subplot2grid(shape=(nrow, ncol), loc=(0, 3), colspan=3, rowspan=1)
+        ax_grism_2d_r = plt.subplot2grid(shape=(nrow, ncol), loc=(1, 3), colspan=3, rowspan=1)
+        ax_contfit = plt.subplot2grid(shape=(nrow, ncol), loc=(0, 6), colspan=3, rowspan=2)
 
-        ax_grism_sub_c = plt.subplot2grid(shape=(nrow, ncol), loc=(2, 2), colspan=3, rowspan=1)
-        ax_grism_sub_r = plt.subplot2grid(shape=(nrow, ncol), loc=(3, 2), colspan=3, rowspan=1)
-        ax_em_c = plt.subplot2grid(shape=(nrow, ncol), loc=(2, 5), colspan=1, rowspan=1)
-        ax_em_r = plt.subplot2grid(shape=(nrow, ncol), loc=(3, 5), colspan=1, rowspan=1)
-        ax_em = plt.subplot2grid(shape=(nrow, ncol), loc=(2, 6), colspan=2, rowspan=2)
+        ax_grism_sub_c = plt.subplot2grid(shape=(nrow, ncol), loc=(2, 3), colspan=3, rowspan=1)
+        ax_grism_sub_r = plt.subplot2grid(shape=(nrow, ncol), loc=(3, 3), colspan=3, rowspan=1)
+        ax_em_c = plt.subplot2grid(shape=(nrow, ncol), loc=(2, 6), colspan=1, rowspan=1)
+        ax_em_r = plt.subplot2grid(shape=(nrow, ncol), loc=(3, 6), colspan=1, rowspan=1)
+        ax_em = plt.subplot2grid(shape=(nrow, ncol), loc=(2, 7), colspan=2, rowspan=2)
 
         # -------get object properites from photcat---------------------
         if df_photcat is not None:
@@ -132,9 +169,15 @@ if __name__ == "__main__":
             sys.exit(f'Photcat file does not exist in {phot_catalog_file}. So where do I get the RA/DEC from?')
 
         # --------cutout and plot the direct image---------------
-        ax_dirimg = cutout2d_and_plot(ra, dec, direct_data, direct_wcs, ax_dirimg, args, cmap=dirimg_cmap)
+        ax_dirimg = cutout2d_and_plot(ra, dec, direct_data, direct_wcs, ax_dirimg, args, cmap=dirimg_cmap, hide_cbar=True)
 
-        # --------cutout and plot the grism images of r & c orients---------------
+        # ----------cutout and plot the grism images of r & c orients---------------
+        ax_grism_dir_c = cutout2d_and_plot(ra, dec, grism_c_data, grism_c_wcs, ax_grism_dir_c, args, cmap=grism_dir_cmap, hide_xaxis=True, hide_yaxis=True, hide_cbar=True)
+        ax_grism_dir_r = cutout2d_and_plot(ra, dec, grism_r_data, grism_r_wcs, ax_grism_dir_r, args, cmap=grism_dir_cmap, hide_xaxis=True, hide_yaxis=True, hide_cbar=True)
+
+        # ----------cutout and plot the grism 2D spectra of r & c orients---------------
+        ax_grism_2d_c = cutout_spectra_and_plot(ra, dec, 'c', grism_c_data, grism_c_wcs, ax_grism_2d_c, args, cmap=grism_2d_cmap)
+        ax_grism_2d_r = cutout_spectra_and_plot(ra, dec, 'r', grism_r_data, grism_r_wcs, ax_grism_2d_r, args, cmap=grism_2d_cmap)
 
         # --------model the 2D continuum and plot---------------
 
