now fixed OIII scaling, and imposed additional z-cut off on sample prior to binning

make_sfms_bins.py
@@ -556,7 +556,7 @@ def bin_SFMS_sfh(df, method_text = '_sfh', delta_sfh=0.2, n_sfh_bins=None, bin_b
     return df, bin_list
 
 # --------------------------------------------------------------------------------------------------------------------
-def read_passage_sed_catalog(filename, use_old=True):
+def read_passage_sed_catalog(filename, use_old=True, z_lim=None):
     '''
     Read the combined master catalog from PASSAGE SED fits, rename a few columns, and only keep the mass and SFR columns
     Return pandas dataframe
@@ -569,24 +569,33 @@ def read_passage_sed_catalog(filename, use_old=True):
         full_df = pd.read_csv(filename, header=0, sep='\t')
         full_df = full_df[full_df['stellar_mass_50'] > 0].reset_index(drop=True) # to get only those sources that have stellar mass measured
 
-    full_df.rename(columns={'Par':'field', 'passage_id':'id', 'id_photcat':'id', 'objid':'id', 'cosmoswebid_1':'cosmosid', 'stellar_mass_50':'log_mass', 'ssfr_50':'log_ssfr', 'sfr_50':'sfr', 'ra_obj':'ra', 'dec_obj':'dec'}, inplace=True)
+    full_df.rename(columns={'Par':'field', 'passage_id':'id', 'id_photcat':'id', 'objid':'id', 'cosmoswebid_1':'cosmosid', 'zbest':'redshift', 'stellar_mass_50':'log_mass', 'ssfr_50':'log_ssfr', 'sfr_50':'sfr', 'ra_obj':'ra', 'dec_obj':'dec'}, inplace=True)
     full_df['log_sfr'] = np.log10(full_df['sfr'])
     
     full_df['delta_tform_50_10'] = full_df['tform50_50'] - full_df['tform10_50']
     full_df['delta_tform_90_50'] = full_df['tform90_50'] - full_df['tform50_50']
     full_df['delta_tform_90_10'] = full_df['tform90_50'] - full_df['tform10_50']
 
-    columns_to_extract = ['field', 'id', 'zbest', 'log_mass', 'log_sfr', 'log_ssfr', 'cosmosid', 'delta_tform_50_10', 'delta_tform_90_50', 'delta_tform_90_10']
+    columns_to_extract = ['field', 'id', 'redshift', 'log_mass', 'log_sfr', 'log_ssfr', 'cosmosid', 'delta_tform_50_10', 'delta_tform_90_50', 'delta_tform_90_10']
     df = full_df[columns_to_extract]
     df['field'] = df['field'].astype(str)
-    df.rename(columns={'zbest':'redshift'}, inplace=True)
+    nobj1 = len(df)
 
     df = df.dropna(subset=['log_mass', 'log_sfr'])
+    nobj2 = len(df)
+    print(f'\nOut of initial {nobj1} objects, {nobj2} had mass available..')
+
+    if z_lim is not None:
+        df = df[df['redshift'].between(z_lim[0], z_lim[1])]
+        nobj3 = len(df)
+        print(f'..out of which {nobj3} made the redshift cut zlim={z_lim}, which is the final sample')
+    else:
+        print(f'..which is the final sample')
 
     return df
 
 # --------------------------------------------------------------------------------------------------------------------
-def get_binned_df(args, skip_binning=False, df=None, method_text='', skip_stacking=False):
+def get_binned_df(args, skip_binning=False, df=None, method_text='', skip_stacking=False, z_lim=None):
     '''
     Read in the full SED dataframe and curtail and bin it as needed to produce the binned dataframe
     Returns binned pandas dataframe, bin_list, args (with added keywords)
@@ -599,11 +608,12 @@ def get_binned_df(args, skip_binning=False, df=None, method_text='', skip_stacki
     args.rescale_text = '_norescale' if args.skip_re_scaling else ''
     args.C25_text = '_wC25' if args.use_C25 and 'NB' not in args.Zdiag else ''
     args.fold_text = '_folded' if args.fold_maps else ''
+    args.zlim_text = '' if z_lim is None else f'_zlim_{z_lim[0]}_{z_lim[1]}'
 
     # ---------reading in the master SED catalog----------------
     if df is None:
         passage_catalog_filename = args.output_dir / 'catalogs' / 'SED_fits_v1.0.2_cosmosweb.fits'
-        df = read_passage_sed_catalog(passage_catalog_filename)
+        df = read_passage_sed_catalog(passage_catalog_filename, z_lim=z_lim)
 
     if args.do_all_fields:
         re_catalog_filename = args.output_dir / f'catalogs/all_fields_re_list.fits'
@@ -625,36 +635,36 @@ def get_binned_df(args, skip_binning=False, df=None, method_text='', skip_stacki
             if args.voronoi_bins:
                 df, bin_summary, centers_scaled, scaling = bin_SFMS_voronoi(df, method_text=method_text, target_n=target_n)
                 bin_list = [bin_summary, centers_scaled, scaling]
-                args.binby_text = f'adap_binby_voronoi_ngal_{target_n}'
+                args.binby_text = f'adap_binby_voronoi_ngal_{target_n}{args.zlim_text}'
             elif args.bin_by_sfh:
                 df, bin_list = bin_SFMS_sfh(df, method_text=method_text, n_sfh_bins=n_sfh_bins) # binning the dataframe in an adaptive way
-                args.binby_text = f'adap_binby_sfh_{n_sfh_bins}'
+                args.binby_text = f'adap_binby_sfh_{n_sfh_bins}{args.zlim_text}'
             elif args.bin_by_distance:
                 df, bin_list = bin_SFMS_distance(df, method_text=method_text, n_adaptive_bins=n_adaptive_bins, sfms=sfms)
-                args.binby_text = f'adap_binby_distance_{n_adaptive_bins}_sfms_{sfms}'
+                args.binby_text = f'adap_binby_distance_{n_adaptive_bins}_sfms_{sfms}{args.zlim_text}'
             elif args.bin_by_distance_mass:
                 df, bin_list = bin_SFMS_distance_mass(df, method_text=method_text, n_adaptive_bins=n_adaptive_bins, sfms=sfms, n_mass_bins=n_mass_bins)
-                args.binby_text = f'adap_binby_distance_{n_adaptive_bins}_sfms_{sfms}_mass_{n_mass_bins}'
+                args.binby_text = f'adap_binby_distance_{n_adaptive_bins}_sfms_{sfms}_mass_{n_mass_bins}{args.zlim_text}'
             else:
                 if args.min_gal_per_bin is not None:
                     df, bin_list = bin_SFMS_adaptive(df, method_text=method_text, min_n=args.min_gal_per_bin) # binning the dataframe in an adaptive way
-                    args.binby_text = f'adap_binby_mass_sfr_mingal_{args.min_gal_per_bin}'
+                    args.binby_text = f'adap_binby_mass_sfr_mingal_{args.min_gal_per_bin}{args.zlim_text}'
                 else:
                     df, bin_list = bin_SFMS_adaptive(df, method_text=method_text, max_n=args.max_gal_per_bin) # binning the dataframe in an adaptive way
-                    args.binby_text = f'adap_binby_mass_sfr_maxgal_{args.max_gal_per_bin}'
+                    args.binby_text = f'adap_binby_mass_sfr_maxgal_{args.max_gal_per_bin}{args.zlim_text}'
         else:
             if args.bin_by_sfh:
                 df, bin_list = bin_SFMS_sfh(df, method_text=method_text, delta_sfh=delta_sfh) # binning the dataframe in an adaptive way
-                args.binby_text = f'lin_binby_sfh_delta_sfh_{delta_sfh}'
+                args.binby_text = f'lin_binby_sfh_delta_sfh_{delta_sfh}{args.zlim_text}'
             elif args.bin_by_distance:
                 df, bin_list = bin_SFMS_distance(df, method_text=method_text, delta_bin=delta_sfms, sfms=sfms)
-                args.binby_text = f'lin_binby_distance_delta_bin_{delta_sfms}_sfms_{sfms}'
+                args.binby_text = f'lin_binby_distance_delta_bin_{delta_sfms}_sfms_{sfms}{args.zlim_text}'
             elif args.bin_by_distance_mass:
                 df, bin_list = bin_SFMS_distance_mass(df, method_text=method_text, delta_bin=delta_sfms, sfms=sfms, n_mass_bins=n_mass_bins)
-                args.binby_text = f'lin_binby_distance_delta_bin_{delta_sfms}_sfms_{sfms}_mass_{n_mass_bins}'
+                args.binby_text = f'lin_binby_distance_delta_bin_{delta_sfms}_sfms_{sfms}_mass_{n_mass_bins}{args.zlim_text}'
             else:
                 df, bin_list = bin_SFMS_linear(df, method_text=method_text) # -binning the dataframe uniformly by mass and SFR bins
-                args.binby_text = f'lin_binby_mass_sfr_delta_mass_{delta_log_mass}_delta_sfr_{delta_log_sfr}'
+                args.binby_text = f'lin_binby_mass_sfr_delta_mass_{delta_log_mass}_delta_sfr_{delta_log_sfr}{args.zlim_text}'
 
         if not args.voronoi_bins:
             if args.bin_by_distance or args.bin_by_distance_mass or args.bin_by_sfh:
@@ -672,7 +682,7 @@ def get_binned_df(args, skip_binning=False, df=None, method_text='', skip_stacki
         args.fits_dir = output_dir / f'maps{args.deproject_text}{args.rescale_text}'
         args.fits_dir.mkdir(parents=True, exist_ok=True)
 
-        args.grad_filename = args.fits_dir / f'stacked{args.binby_text}{args.fold_text}_fits_allbins_Zdiag_{args.Zdiag}{args.C25_text}{args.deproject_text}{args.rescale_text}.fits'
+        args.grad_filename = args.fits_dir / f'stacked{args.binby_text}{args.fold_text}_fits_allbins_Zdiag_{args.Zdiag}{args.C25_text}{args.deproject_text}{args.rescale_text}{args.zlim_text}.fits'
 
         # ---------merge with effective radius catalog--------------------
         if not skip_stacking:
@@ -728,10 +738,11 @@ log_mass_bins = np.arange(6.5, 11.5 + delta_log_mass/2, delta_log_mass)
 log_sfr_bins = np.arange(-2.5, 2.5 + delta_log_sfr/2, delta_log_sfr)
 
 # ---------------choose which binning methods to try---------------
-methods = [#'adaptive_nmin', \
+methods = [
+            # 'adaptive_nmin', \
             # 'adaptive_nmax', \
             # 'adaptive_voronoi', \
-            'adaptive_distance', \
+            # 'adaptive_distance', \
             'adaptive_distance_mass', \
             # 'adaptive_sfh', \
             # 'linear', \
@@ -748,6 +759,10 @@ sfms =  'Popesso23' # from 'Popesso23', 'Shivaei15' and 'Whitaker14'; for binnin
 delta_sfh = 0.5 # delta in tform90 - tform10 parameter (in Gyr) in which to bin, unless binning adaptively
 n_sfh_bins = 10 # number of bins in SFH parameter, adaptive, so that each bin contains approximately equal number of objects
 
+#z_lim = [0.06, 2.35] # for having OIII and Ha
+z_lim = [1.15, 2.35] # for having OII and OIII and Ha
+#z_lim = None # no redshift cut
+
 # --------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     args = parse_args()
@@ -757,7 +772,7 @@ if __name__ == "__main__":
 
     # ---------reading in the master SED catalog----------------
     passage_catalog_filename = args.output_dir / 'catalogs' / 'SED_fits_v1.0.2_cosmosweb.fits'
-    df = read_passage_sed_catalog(passage_catalog_filename)
+    df = read_passage_sed_catalog(passage_catalog_filename, z_lim=z_lim)
 
     # -----------making bins in various ways---------------------
     for method in methods:
@@ -772,7 +787,7 @@ if __name__ == "__main__":
         elif 'distance' in method: args.bin_by_distance = True
         if '_nmin' in method and args.min_gal_per_bin is None: args.min_gal_per_bin = 20
 
-        df, bin_list, args = get_binned_df(args, df=df, method_text=f'_{method}', skip_stacking=True)
+        df, bin_list, args = get_binned_df(args, df=df, method_text=f'_{method}', skip_stacking=True, z_lim=z_lim)
 
         if 'voronoi' in method:
             bin_summary, centers_scaled, scaling = bin_list

plot_stacked_maps.py
@@ -20,7 +20,7 @@
 from header import *
 from util import *
 from make_diagnostic_maps import compute_Z_C19, compute_Z_KD02_R23, compute_Z_P25, compute_Z_Te, compute_Te, take_safe_log_ratio, take_safe_log_sum, myimshow, get_AGN_func_methods
-from make_sfms_bins import get_binned_df
+from make_sfms_bins import get_binned_df, z_lim
 from stack_emission_maps import read_stacked_maps
 from plots_for_zgrad_paper import plot_fitted_line, odr_fit, plot_AGN_demarcation_ax, get_distance_map_from_AGN_line, get_ratio_labels, overplot_AGN_line_on_BPT
 
@@ -465,11 +465,11 @@ def compute_Z_NB(line_label_array, line_waves_array, line_flux_array):
                     N2Ha_model = best_model_dict['table'].loc['NII6583', 'Model'] / best_model_dict['table'].loc['Halpha', 'Model']
                     print(f'N2Ha = {N2Ha_model}')
             
+                counter += 1
             else:
                 logOH = ufloat(np.nan, np.nan)
                 print(f'Could not run NB for unique ID {this_ID} ({counter + 1} out of {len(unique_IDs_array)}) with only {len(obs_fluxes)} good fluxes')
 
-            counter += 1
             logOH_dict_unique_IDs.update({this_ID: logOH}) # updating to unique ID dictionary once logOH has been calculated for this unique ID
 
         logOH_array.append(logOH)
@@ -1025,7 +1025,7 @@ if __name__ == "__main__":
     args.fontfactor = 1.5
 
     # ------------reading and binning dataframe-------------
-    df, bin_list, args = get_binned_df(args)
+    df, bin_list, args = get_binned_df(args, z_lim=z_lim)
     
     # ------------setting up master dataframe----------------------
     common_cols = ['nobj', 'logOH_int', 'logOH_int_u', 'minor_logOH_grad', 'minor_logOH_grad_u', 'major_logOH_grad', 'major_logOH_grad_u', 'radial_logOH_grad', 'radial_logOH_grad_u']
@@ -1050,7 +1050,7 @@ if __name__ == "__main__":
             bin_text = f'delta_sfms_bin_{this_delta_sfms_bin.left}-{this_delta_sfms_bin.right}_mass_bin_{this_mass_bin.left}-{this_mass_bin.right}'
         else:
             bin_text = f'logmassbin_{this_bin[0].left}-{this_bin[0].right}_logsfrbin_{this_bin[1].left}-{this_bin[1].right}'
-        print(f'\tStarting ({index2 + 1}/{len(bin_list)}) {bin_text}..', end=' ')
+        print(f'\n\tStarting ({index2 + 1}/{len(bin_list)}) {bin_text}..', end=' ')
         
         # -------reading previously saved stacked fits file------------
         stacked_filename = args.fits_dir / f'stacked_maps{args.deproject_text}{args.rescale_text}_{bin_text}.fits'
@@ -1058,6 +1058,9 @@ if __name__ == "__main__":
             print(f'No stacked fits file found for {bin_text}, so skipping this bin.')
             continue
         line_dict = read_stacked_maps(stacked_filename, args)
+        if not line_dict:
+            print(f'No lines found for {bin_text}. So Skipping.')
+            continue
         nbin_good += 1
 
         # ---------fold stacked maps along major and minor axis--------------------
@@ -1070,9 +1073,6 @@ if __name__ == "__main__":
         # ---------------plot emission line maps of this bin---------------------
         if args.plot_line_maps:
             fig_em, line_list = plot_stacked_line_maps(line_dict, args, bin_text=bin_text, takelog=True, cmin=-3, cmax=-2)
-            if len(line_list) == 0:
-                print(f'No lines found for {bin_text}. So Skipping.')
-                continue
             save_fig(fig_em, args.fig_dir, f'stacked{args.fold_text}_line_maps{args.deproject_text}{args.rescale_text}_{bin_text}.png', args) # saving the figure
 
         # -----------------computing metallicity maps of this bin---------------
@@ -1098,12 +1098,6 @@ if __name__ == "__main__":
             fig_met, line_list, minor_linefit_odr, major_linefit_odr, radial_linefit_odr = plot_line_and_metallicity_maps(line_dict, logOH_map, args, bin_text=bin_text, takelog=True, cmin=-3, cmax=-2, Zmin=None, Zmax=None)
             save_fig(fig_met, args.fig_dir, f'stacked{args.fold_text}_line_and_metallicity_map_{args.Zdiag}{args.C25_text}{args.deproject_text}{args.rescale_text}_{bin_text}.png', args) # saving the figure
 
-        # -----------------plot line maps and metallicity maps of this bin---------------
-        if args.plot_line_and_bpt:
-            fig_bpt = plot_line_and_bpt_maps(line_dict, args, bin_text=bin_text, takelog=True, cmin=-3.5, cmax=-2.5)
-            if fig_bpt is not None:
-                save_fig(fig_bpt, args.fig_dir, f'stacked{args.fold_text}_line_and_bpt_map_{args.AGN_diag}{args.deproject_text}{args.rescale_text}_{bin_text}.png', args) # saving the figure
-
         # -------------save fit results to dataframe-----------------------
         if args.plot_radial_profiles and (args.plot_metallicity or args.plot_line_and_metallicity):
             if args.bin_by_distance_mass:
@@ -1113,17 +1107,23 @@ if __name__ == "__main__":
             else:
                 thisrow = {'log_mass_bin':this_bin[0], 'log_sfr_bin':this_bin[1]}
             
-            thisrow.update({'nobj':nobj, \
-                            'logOH_int':logOH_int.n, \
-                            'logOH_int_u':logOH_int.s, \
-                            'minor_logOH_grad':minor_linefit_odr[0].n, \
-                            'minor_logOH_grad_u':minor_linefit_odr[0].s, \
+            thisrow.update({'nobj': nobj, \
+                            'logOH_int': logOH_int.n, \
+                            'logOH_int_u': logOH_int.s, \
+                            'minor_logOH_grad': minor_linefit_odr[0].n, \
+                            'minor_logOH_grad_u': minor_linefit_odr[0].s, \
                             'major_logOH_grad': major_linefit_odr[0].n, \
                             'major_logOH_grad_u': major_linefit_odr[0].s, \
                             'radial_logOH_grad': radial_linefit_odr[0].n, \
                             'radial_logOH_grad_u': radial_linefit_odr[0].s, \
                             })
             df_grad.loc[len(df_grad)] = thisrow
+
+        # -----------------plot line maps and metallicity maps of this bin---------------
+        if args.plot_line_and_bpt:
+            fig_bpt = plot_line_and_bpt_maps(line_dict, args, bin_text=bin_text, takelog=True, cmin=-3.5, cmax=-2.5)
+            if fig_bpt is not None:
+                save_fig(fig_bpt, args.fig_dir, f'stacked{args.fold_text}_line_and_bpt_map_{args.AGN_diag}{args.deproject_text}{args.rescale_text}_{bin_text}.png', args) # saving the figure
 
         if len(bin_list) > 5: plt.close('all')
         print(f'\nCompleted bin {bin_text} in {timedelta(seconds=(datetime.now() - start_time3).seconds)}, {len(bin_list) - index2 - 1} to go!')

stack_emission_maps.py
@@ -19,7 +19,7 @@ from header import *
 from util import *
 setup_plot_style()
 from make_diagnostic_maps import trim_image, get_dereddened_flux, myimshow, get_offsets_from_center, get_cutout
-from make_sfms_bins import get_binned_df
+from make_sfms_bins import get_binned_df, z_lim
 
 start_time = datetime.now()
 
@@ -400,13 +400,14 @@ if __name__ == "__main__":
     cmin, cmax, cmap = -3.4, -1.4, 'cividis'
     
     # ------------reading and binning dataframe-------------
-    df, bin_list, args = get_binned_df(args)
+    df, bin_list, args = get_binned_df(args, z_lim=z_lim)
     n_lines = len(args.line_list) + 1 # one additional column for the direct image
 
     if args.id is not None: df = df[df['id'].isin(args.id)]
     
     # ------------looping over each bin-----------------------
     nbin_good = 0
+    nobj_total_binned = 0
 
     for index2, this_mass_sfr_bin in enumerate(bin_list):
         if args.debug_bin and nbin_good > 0: break
@@ -444,6 +445,7 @@ if __name__ == "__main__":
             nbin_good += 1
             nobj_good = 0
             ngal_this_bin = len(ids)
+            nobj_no_scale_line = 0
         else:
             # --------determine which objects fall in this bin----------
             if args.bin_by_distance_mass:
@@ -458,13 +460,13 @@ if __name__ == "__main__":
             nobj_good = 0
             ngal_this_bin = len(ids_in_thisbin)
             nrows_total = ngal_this_bin + 1 # one extra row for the stacked plots
+            nobj_no_scale_line = 0
             
             if ngal_this_bin > 0:
                 print(f'which has {ngal_this_bin} objects.')
                 line_maps_array = [[] for _ in range(len(args.line_list))] # each array must be len(args.line_list) long and each element will become a stack of 2D images
                 line_maps_err_array = [[] for _ in range(len(args.line_list))] # each array must be len(args.line_list) long and each element will become a stack of 2D images
                 constituent_ids_array = [[] for _ in range(len(args.line_list))]
-                total_brightness_array = [[] for _ in range(len(args.line_list))]
 
                 # --------setup PDF for mammoth figures for all emission line map plots----------
                 if not args.debug_align:
@@ -493,16 +495,14 @@ if __name__ == "__main__":
                     # ----------------looping over the objects in this chunk-------------
                     for index3, this_galaxy in enumerate(chunk):
                         obj = df.loc[df['id'] == this_galaxy].iloc[0]
-                        print(f'\t\tCommencing ({chunk_id + index3 + 1}/{ngal_this_bin}) ID {this_galaxy}..')
                         args.field = obj['field']
                         args.id = obj['id']
+                        print(f'\n\t\tCommencing ({chunk_id + index3 + 1}/{ngal_this_bin}) ID {args.field}:{this_galaxy}..')
 
                         # ------determining directories and filenames---------
                         product_dir = args.input_dir / args.field / 'Products'
                         full_fits_file = product_dir / 'full_psf_matched' / f'{args.field}_{args.id:05d}.full.fits'
                         maps_fits_file = product_dir / 'maps_psf_matched' / f'{args.field}_{args.id:05d}.maps.fits'
-                        # full_fits_file = product_dir / 'full' / f'{args.field}_{args.id:05d}.full.fits'
-                        # maps_fits_file = product_dir / 'maps' / f'{args.field}_{args.id:05d}.maps.fits'
                         od_filename = product_dir / 'spec1D' / f'{args.field}_{args.id:05d}.1D.fits'
 
                         if os.path.exists(maps_fits_file): # if the fits files are in maps/
@@ -552,10 +552,16 @@ if __name__ == "__main__":
                         if scaling_line in args.available_lines:
                             _, _, line_int = get_emission_line_map(scaling_line, full_hdu, args, dered=False, silent=True)
                             integrated_scaling_flux = line_int.n
-                            print(f'\nScaling all line fluxes of {args.id} by {scaling_line} flux = {integrated_scaling_flux}')
+                            if integrated_scaling_flux > 0:
+                                print(f'Scaling all line fluxes of {args.id} by {scaling_line} flux = {integrated_scaling_flux}')
+                            else:
+                                print(f'Since integrated flux for {scaling_line} < 0 (= {integrated_scaling_flux}), for {args.id}, not putting this in stack')
+                                nobj_no_scale_line += 1
+                                continue
                         else:
-                            integrated_scaling_flux = 1e-17 # dummy value for galaxies that do not have OIII-5007 coverage
-                            print(f'\nSince {scaling_line} not available for {args.id}, scaling all lines by {integrated_scaling_flux}')
+                            print(f'Since {scaling_line} not available for {args.id}, not putting this in stack')
+                            nobj_no_scale_line += 1
+                            continue
                         
                         # ---------------direct image---------------
                         filter = 'F200W'             
@@ -569,8 +575,6 @@ if __name__ == "__main__":
                         recentered_direct_image = ndimage.shift(rescaled_direct_image, [args.ndelta_xpix_rescaled, args.ndelta_ypix_rescaled], order=0, cval=np.nan)
 
                         recentered_segmentation_map = ndimage.shift(rescaled_segmentation_map, [args.ndelta_xpix_rescaled, args.ndelta_ypix_rescaled], order=0, cval=np.nan)
-
-                        total_brightness = np.nansum(direct_image)
 
                         # ---------plotting direct image and the rescaled direct image------------------------------
                         if not args.debug_align:
@@ -608,16 +612,9 @@ if __name__ == "__main__":
                                 line_map /= integrated_scaling_flux
                                 line_map_err /= integrated_scaling_flux
 
-                                # ------smoothing the map before rotating--------
-                                # smoothing_kernel = Box2DKernel(args.kernel_size, mode=args.kernel_mode)
-                                # smoothed_line_map = convolve(line_map, smoothing_kernel)
-                                # smoothed_line_map_err = convolve(line_map_err, smoothing_kernel)
-
                                 # --------rotating the line map---------------------
                                 rotated_line_map = rotate_line_map(line_map, args)
                                 rotated_line_map_err = rotate_line_map(line_map_err, args)
-                                # rotated_line_map = rotate_line_map(smoothed_line_map, args)
-                                # rotated_line_map_err = rotate_line_map(smoothed_line_map_err, args)
 
                                 # --------deprojecting the line map---------------------
                                 deprojected_line_map = rotated_line_map if args.skip_deproject else deproject_line_map(rotated_line_map, args)
@@ -643,7 +640,7 @@ if __name__ == "__main__":
                                     axes[3] = myimshow(rescaled_line_map, axes[3], contour=rescaled_segmentation_map != args.id, re_pix=args.npix_side / (2 * args.re_limit), label='Rescaled', cmap=cmap, col='w' if args.fortalk else 'k')
                                     axes[4] = myimshow(recentered_line_map, axes[4], contour=recentered_segmentation_map != args.id, re_pix=args.npix_side / (2 * args.re_limit), label='Recentered', cmap=cmap, col='w' if args.fortalk else 'k')
 
-                                    figname = fig_dir / f'debug_align_{args.id}{args.deproject_text}{args.rescale_text}.png'
+                                    figname = args.fig_dir / f'debug_align_{args.id}{args.deproject_text}{args.rescale_text}.png'
                                     fig_debug.savefig(figname, transparent=args.fortalk)
                                     print(f'\nSaved figure as {figname}')
 
@@ -654,7 +651,6 @@ if __name__ == "__main__":
                                 line_maps_array[index4].append(recentered_line_map)
                                 line_maps_err_array[index4].append(recentered_line_map_err)
                                 constituent_ids_array[index4].append(f'{args.field}-{args.id}')
-                                total_brightness_array[index4].append(total_brightness)
 
                                 nlines_good += 1
 
@@ -686,7 +682,7 @@ if __name__ == "__main__":
                 # -----stacking all line maps for all objects in this bin-----------
                 stacked_maps, stacked_maps_err, nobj_arr = [], [], []
                 for index4, this_line in enumerate(args.line_list):
-                    stacked_map, stacked_map_err = weighted_stack_line_maps(line_maps_array[index4], line_maps_err_array[index4])#, additional_weights= 1 / np.array(total_brightness_array[index4]) ** 2)
+                    stacked_map, stacked_map_err = weighted_stack_line_maps(line_maps_array[index4], line_maps_err_array[index4])
                     stacked_maps.append(stacked_map)
                     stacked_maps_err.append(stacked_map_err)
                     nobj_arr.append(len(constituent_ids_array[index4]))
@@ -724,7 +720,9 @@ if __name__ == "__main__":
                 print(f'which has no object. Skipping this bin.')
                 continue
 
-        print(f'\nCompleted bin {bin_text} ({nobj_good} / {ngal_this_bin} objects) in {timedelta(seconds=(datetime.now() - start_time3).seconds)}, {len(bin_list) - index2 - 1} to go!')
+        nobj_total_binned += nobj_good
+        print(f'\nCompleted bin {bin_text} ({nobj_good} / {ngal_this_bin} objects, {nobj_no_scale_line} skipped due to lack of {scaling_line}) in {timedelta(seconds=(datetime.now() - start_time3).seconds)}, {len(bin_list) - index2 - 1} to go!')
         if nobj_good > 1: nbin_good += 1
 
+    print(f'\nBinned total ({nobj_total_binned} / {len(df)}) objects, into {len(bin_list)} bins.')
     print(f'Completed in {timedelta(seconds=(datetime.now() - start_time).seconds)}')